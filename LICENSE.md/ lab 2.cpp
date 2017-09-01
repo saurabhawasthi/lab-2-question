93274be151b1bc84775afe006119df9eeca36e4d
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 19:41:05 +0530

    Add files via upload

---
+++ LICENSE.md/ lab 2.cpp
@@ -0,0 +1,12 @@
+#include <iosterem>
+using namespacestd;
+{
+float l,m,km;
+cout<<"\nenter length in cm =";
+cin>>l;
+m=l/100;
+km=l/100000;
+cout<<"\n length in metre="<<m;
+cout<<"\n length in kilometre="<<km;
+return 0;
+}
